de94881b698cb9453219e9ef26db049ca965d271
Aftermath001 <alvinasiachi@gmail.com>
2024-03-07 13:58:44 +0300

    Featured Products

--- client/client/src/pages/Home/Home.jsx
+++ client/client/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import  Slider  from '../../components/Slider/Slider'
 import React from 'react'
 import './Home.scss'
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 
 
 const Home = () => {
   return (
    <div className="home">
-      <Slider/>
+      <Slider />
+      <FeaturedProducts type='Featured'/>
+      <FeaturedProducts type='Trending'/>
    </div>
   )
 }
